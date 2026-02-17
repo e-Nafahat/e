64169e28c7cda3cbfaa1898051a9271215f3e734
e-Nafahat <myway1661@gmail.com>
2026-02-17 20:36:43 -0300

    Change cache name and fetch strategy in sw.js
    Updated cache name and modified fetch strategy to use network first approach.

--- sw.js
+++ sw.js
@@ -1,47 +1,7 @@
-const CACHE_NAME = 'nafahat-v5';
-
-const ASSETS_TO_CACHE = [
-  '/e/',
-  '/e/index.html',
-  '/e/manifest.json',
-  '/e/robots.txt',
-  '/e/sitemap.xml',
-  '/e/.nojekyll',
-
-  // leader
-  '/e/leader/home.html',
-  '/e/leader/admin.html',
-  '/e/leader/FrontSmart.html',
-  '/e/leader/more.html',
-  '/e/leader/masbaha.html',
-  '/e/leader/intro.html',
-  '/e/leader/khatma.html',
-  '/e/leader/Mehrab.html',
-  '/e/leader/support.html',
-  '/e/leader/Nresearch.html',
-  '/e/leader/live.html',
-  '/e/leader/main.js',
-  '/e/leader/masbaha_script.js',
-  '/e/leader/masbaha_style.css',
-  '/e/leader/style.css',
-  '/e/leader/LogoNafahat.png',
-  '/e/leader/lindo.mp3',
-
-  // Library
-  '/e/Library/library.html',
-
-  // Textbook
-  '/e/Textbook/nawawi.html',
-  '/e/Textbook/hadith100.html'
-];
+const CACHE_NAME = 'nafahat-dynamic-v2';
 
 // Install
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
   self.skipWaiting();
 });
 
@@ -50,36 +10,33 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
+        cacheNames.map((cache) => caches.delete(cache))
       );
     }).then(() => self.clients.claim())
   );
 });
 
-// Fetch Strategy: Stale While Revalidate
+// Fetch – Network First (تحديث سريع جداً)
 self.addEventListener('fetch', (event) => {
+
   if (event.request.method !== 'GET') return;
 
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
+    fetch(event.request)
+      .then((response) => {
 
-      const fetchPromise = fetch(event.request)
-        .then((networkResponse) => {
-          if (networkResponse && networkResponse.status === 200) {
-            const responseClone = networkResponse.clone();
-            caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, responseClone);
-            });
-          }
-          return networkResponse;
-        })
-        .catch(() => cachedResponse);
+        const responseClone = response.clone();
 
-      return cachedResponse || fetchPromise;
-    })
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
+
 });
